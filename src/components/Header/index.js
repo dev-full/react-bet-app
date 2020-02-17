3216629fa10cd34d5dc7707b28fcc10d02a09586
BrightStar <cooldev3324@gmail.com>
2020-02-17 09:56:35 -0500

    Header bar

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+const Header = () => {
+  return (
+    <div className="navbar navbar-top-fixed">
+      <div className="navbar-right" />
+    </div>
+  );
+};
+
+export default Header;
